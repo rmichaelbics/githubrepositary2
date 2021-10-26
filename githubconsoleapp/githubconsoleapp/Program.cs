a75b769aa883d27d31c9a0d54126643fb47dafa0
MonishaParthiban <monishaparthibanvkl@gmail.com>
2021-10-26 17:14:38 +0530

    Added another message

--- githubconsoleapp/githubconsoleapp/Program.cs
+++ githubconsoleapp/githubconsoleapp/Program.cs
@@ -11,6 +11,7 @@ namespace githubconsoleapp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine("Hello Everyone");
         }
     }
 }
